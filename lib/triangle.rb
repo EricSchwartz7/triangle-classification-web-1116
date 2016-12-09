ffbab89f16f43412813a32c85ba4316df4d6c522
github username <github email address>
2016-12-08 23:59:48 -0500

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  attr_reader :length, :width, :height
+  attr_accessor :type
+
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+    if valid?
+      if length == width && length == height
+        type = :equilateral
+      elsif length == width || length == height || width == height
+        type = :isosceles
+      elsif length != width && length != height && height != width
+        type = :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+
+  def valid?
+    length > 0 && width > 0 && height > 0 && length + width > height && length + height > width && width + height > length
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "This triangle is invalid!"
+  end
 end
